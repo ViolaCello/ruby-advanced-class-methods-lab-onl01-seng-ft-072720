fe4e31442c7c2c65759314580be2f415c1dfca5f
Anthony Lepore <violacellonyc@gmail.com>
2020-08-05 17:31:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -42,7 +42,7 @@ end
 def self.find_or_create_by_name(name)
   part_one = self.find_by_name(name)
   if part_one == false 
-    self.create_by_name(name)
+    part_two = self.create_by_name(name)
   else part_one 
   end
 end
